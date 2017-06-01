03f15a3661aa6172f98c00eed9be16b5040f105d
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-05-31 22:10:45 -0700

    inital commit - doesn't do anything yet

---
+++ scraper.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3.5
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def scrape(self):
+        pass
+
+
+def main():
+    s = Scraper()
+    s.scrape()
+
+
+if __name__ == "__main__":
+    main()

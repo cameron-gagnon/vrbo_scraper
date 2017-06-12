ad5bad78fabcc1b299ab62838c6f128e2e45abe5
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-06-12 02:02:33 -0700

    make it easier to generate master files with helper script

--- create_master_files.sh
+++ create_master_files.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+listing_counter=0
+review_counter=0
 for filename in **/**; do
     if [[ $filename == *"review"* ]]; then
         echo "processing review for $filename"
-        awk 'NR>1 { print $0 }' $filename >> master_reviews.csv
+        if [[ $review_counter -ge 1 ]]; then
+            awk 'NR>1 { print $0 }' $filename >> master_reviews.csv
+        else
+            echo "Using file to create initial reviews"
+            awk '{ print $0 }' $filename > master_reviews.csv
+        fi
+        ((review_counter=review_counter+1))
     elif [[ $filename == *"listing"* ]]; then
         echo "processing listing for $filename"
-        awk 'NR>1 { print $0 }' $filename >> master_listings.csv
+        if [[ $listing_counter -ge 1 ]]; then
+            awk 'NR>1 { print $0 }' $filename >> master_listings.csv
+        else
+            echo "Using file to create initial listings"
+            awk '{ print $0 }' $filename > master_listings.csv
+        fi
+        ((listing_counter=listing_counter+1))
     fi
 done
